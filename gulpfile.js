f40e1478e4ed86ca2019a6f4e8fc5d741ac800c3
vicguley <80715331+vicguley@users.noreply.github.com>
2021-04-06 10:58:38 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
 const gulp = require("gulp");
+const concat = require("gulp-concat");
 const { dest, src } = require("gulp");
 
 const task1 = async () => {
-  return src("./src/*.js").pipe(dest("output/result.js"))
+  return src("./src/*.js")
+  .pipe(concat("result.js"))
+  .pipe(dest("./output"));
 };
 
 const task2 = async () => {
-  return src("./src/*.css").pipe(dest("output/result.css"))
+  return src("./src/*.css")
+  .pipe(concat("result.css"))
+  .pipe(dest("./output"))
 }
 
 const bothTasks = gulp.parallel(task1, task2);
